50d7eb11c6b5530a91a584111efb0a2ef298f1ab
Amethyst-Kana617 <77530457+Amethyst-Kana617@users.noreply.github.com>
2024-08-27 20:03:06 +0800

    main

---
+++ 瞎写八写/try c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int double_list[3][3] ={{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        printf("%d\n",double_list[i][j]);
+    }
+}
+return 0;
+}
