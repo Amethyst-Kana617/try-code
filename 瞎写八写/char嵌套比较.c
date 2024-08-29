72b85d22f1743b59caed987ff0efc6acdebc2bf7
Amethyst-Kana617 <77530457+Amethyst-Kana617@users.noreply.github.com>
2024-08-29 23:55:16 +0800

    main

---
+++ 瞎写八写/char嵌套比较.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char a = 'b';
+    char b = 'b';
+    if( a == 'b' )
+    {
+        printf("A=A\n");
+        if (a=='a')
+        {
+            printf("A=B\n");
+        }
+        
+    }
+    return 0;
+}
---
+++ 瞎写八写/char循环比较.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char a = 'c';
+    char b = 'b';
+    char c = 'a';
+    for(a = 'a'; a <= 'c'; a++) {
+        printf("%c ", a);
+    }
+}
